083ea3ab867743ac125a7abf15fa0a04cae5dff4
Edsel Lopez <edsel.lopez@jmaconsulting.biz>
2020-05-29 16:41:32 +0530

    Fixed disclaimer links

--- src/Form/DisclaimerForm.php
+++ src/Form/DisclaimerForm.php
@@ -31,7 +31,7 @@
 <div class=\"content\">
 <p>Il est important d’effectuer votre propre recherche et de prendre vos propres décisions éclairées. Veuillez noter qu’Autisme Ontario ne cautionne aucune thérapie, aucun produit, aucun traitement, aucune stratégie, aucune opinion, aucun service ou aucune personne en particulier. Nous appuyons cependant votre droit à l’information.</p>
 
-<p>Cliquez <a data-entity-substitution=\"file\" data-entity-type=\"file\" data-entity-uuid=\"aaa34767-aa89-4fc6-bd83-bda3c6d6286b\" href=\"/sites/default/files/2020-05/Site%20Disclaimer_Christa_BIL.pdf\">ici</a> pour lire l’avertissement d’Autisme Ontario dans son intégralité.</p>
+<p>Cliquez <a target=\"_blank\" href=\"https://www.autismontario.com/fr/avertissements-et-limitations-de-responsabilite\">ici</a> pour lire l’avertissement d’Autisme Ontario dans son intégralité.</p>
 
 <p>";
         $acknowledgement = "Je reconnais que j’ai lu les avertissements et les limitations de responsabilité d’Autisme Ontario.";
@@ -41,7 +41,7 @@
 <div class=\"content\">
 <p>It is important to do your own research and make your own informed decisions. Please note that Autism Ontario does not endorse any specific therapy, product, treatment, strategy, opinions, service, or individual. We do, however, endorse your right to information.</p>
 
-<p>Click <a data-entity-substitution=\"file\" data-entity-type=\"file\" data-entity-uuid=\"aaa34767-aa89-4fc6-bd83-bda3c6d6286b\" href=\"/sites/default/files/2020-05/Site%20Disclaimer_Christa_BIL.pdf\">here</a> to read Autism Ontario’s full disclaimer.</p>
+<p>Click <a target=\"_blank\" href=\"https://www.autismontario.com/disclaimer\">here</a> to read Autism Ontario’s full disclaimer.</p>
 
 <p>";
         $acknowledgement = "I acknowledge that I have read Autism Ontario’s disclaimers and limitations of liability.";
